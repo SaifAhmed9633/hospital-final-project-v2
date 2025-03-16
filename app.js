6674b38a12ad6a7be4594212bf2b461b5ae9efd7
SaifAhmed9633 <71185175+SaifAhmed9633@users.noreply.github.com>
2025-03-16 14:51:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+    function Addrecord() {
+        const table = document.getElementById("mytable").getElementsByTagName('tbody')[0];
+        const fullName = document.getElementById("FULLNAME").value;
+        const email = document.getElementById("EMAIL").value;
+        const donationAmount = document.getElementById("MONEY").value;
+
+        const row = table.insertRow(-1);
+        row.insertCell(0).textContent = fullName;
+        row.insertCell(1).textContent = email;
+        row.insertCell(2).textContent = "$" + donationAmount;
+
+        document.getElementById("FULLNAME").value = "";
+        document.getElementById("EMAIL").value = "";
+        document.getElementById("MONEY").value = "";
+        document.getElementById()
+        alert(`Thank you Mr. ${fullName} for your donation of $${donationAmount} to our hospital!`);
+    }
